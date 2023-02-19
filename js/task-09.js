621be1ab09a3e9d83277828b323ad6a0a3430432
Trehub Anatolii <tridfoto@gmail.com>
2023-02-19 23:11:48 +0200

    add task 9 and 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const but = document.querySelector('button.change-color');
+const ev = but.addEventListener('click', () => {
+  const bodyColor = getRandomHexColor();
+  document.querySelector('body').style.backgroundColor = bodyColor;
+  document.querySelector('span.color').textContent = bodyColor;
+})
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount){
+if(amount.value > 0){
+  let boxWidth = 0;
+  let boxHeight = 0;
+  let bgcolor = '';
+  for(let i=0; i<amount.value; i++){
+    bgcolor = getRandomHexColor();
+    boxWidth = 30 + i * 10;
+    boxHeight = 30 + i * 10;
+
+    const newBox = document.createElement('div');
+    newBox.style=`width:${boxWidth}px; height:${boxHeight}px; background-color:${bgcolor}`;
+    document.querySelector('div#boxes').appendChild(newBox);
+  }
+}
+}
+
+function destroyBoxes(){
+  document.querySelector('div#boxes').innerHTML = '';
+}
+
+const inp = document.querySelector('#controls').firstElementChild;
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+btnCreate.addEventListener('click', () => createBoxes(inp));
+btnDestroy.addEventListener('click', () => destroyBoxes());
